f4f8d3ea68763d807cacfafd1162e613cea6e5b8
MargulisMax <margulismax@gmail.com>
2018-11-12 21:42:05 +0200

    Fixed: 1. Added flags to click options 2. Shortened my code

--- home-assignments/session2/cli.py
+++ home-assignments/session2/cli.py
@@ -8,30 +8,14 @@ def print_weather(p_city, p_weather, p_unit, p_days):
     for day in range(int(p_days)+1):
         if day == 0:
             print('The weather in ' + p_city + ' today is ' + weather_condition +
-            ' with temperatures trailing from ' + weather_info[day].low + '-' + weather_info[day].high + ' ' + p_unit)
+            ' with temperatures trailing from ' + weather_info[day].low + '-' + weather_info[day].high + ' ' +
+                  p_unit.lower())
         else:
             if count == 0:
                 print('Forecast for the next ' + p_days + ' days')
                 count += 1
             print(weather_info[day].date + ' ' + weather_condition + ' with temperatures trailing from ' +
-                  weather_info[day].low + '-' + weather_info[day].high + ' ' + p_unit)
-
-def unit_celsius():
-    weather = Weather(unit=Unit.CELSIUS)
-    unit_type = 'Celsius'
-    return weather, unit_type
-
-def unit_fahrenheit():
-    weather = Weather(unit=Unit.FAHRENHEIT)
-    unit_type = 'Fahrenheit'
-    return weather, unit_type
-
-def set_forecast_unit(unit_input):
-    if unit_input == '-c':
-        set_unit = unit_celsius()
-    elif unit_input == '-f':
-        set_unit = unit_fahrenheit()
-    return set_unit
+                  weather_info[day].low + '-' + weather_info[day].high + ' ' + p_unit.lower())
 
 def parse_forecast_days_range(mixed_range):
     if '+' in mixed_range:
@@ -42,16 +26,19 @@ def parse_forecast_days_range(mixed_range):
 
 @click.command()
 @click.option('--city', help='City for which forecast is needed')
-@click.option('--forecast', type=(str, str), help='Time range for the forecast,'
-                                                  'Options: TODAY, TODAY+X(X - stands for number of days)')
+@click.option('--forecast', help='Time range for the forecast.\n'
+                                'Options:\n '
+                                '1.TODAY.\n'
+                                '2.TODAY+X(X-Number of days,'
+                                 'Max number of days is 9)')
+@click.option('-c', 'forecast_unit', flag_value='CELSIUS', help='Show temperature in Celsius')
+@click.option('-f', 'forecast_unit', flag_value='FAHRENHEIT', help='Show temperature in Fahrenheit')
 
-def get_weather(city, forecast):
+def get_weather(city, forecast, forecast_unit):
     """forecast for the next X days based on city and forecast(date) parameters."""
-    forecast_time_range = parse_forecast_days_range(forecast[0])
-    chosen_unit = set_forecast_unit(forecast[1])
-    city_weather_info = chosen_unit[0].lookup_by_location(city)
-    print_weather(city, city_weather_info, chosen_unit[1], forecast_time_range)
-
+    forecast_time_range = parse_forecast_days_range(forecast)
+    city_weather_info = Weather(unit=getattr(Unit, forecast_unit)).lookup_by_location(city)
+    print_weather(city, city_weather_info, forecast_unit, forecast_time_range)
 
 if __name__ == '__main__':
     get_weather()
