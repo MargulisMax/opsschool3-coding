aaa044c4c4453eb465b48964a2b3bec7c82302f7
MargulisMax <margulismax@gmail.com>
2018-12-15 23:31:02 +0200

    Added response and tags

--- home-assignments/boto/boto3_exercise1.py
+++ home-assignments/boto/boto3_exercise1.py
@@ -1,16 +1,30 @@
 import boto3
 import click
 
+def add_tags(vpc):
+    new_tags = vpc.create_tags(
+        Tags=[
+            {
+                'Key': 'Name',
+                'Value': 'Boto3-Exercise1'
+            }
+        ]
+    )
+    print(new_tags)
+
 @click.command()
 @click.option('--cidr', help='Define network address range in a format of x.x.x.x/x')
 @click.option('--region', help='Enter the region where VPC will be created')
 @click.option('--tenancy', help='choose Default for shared host or dedicated for a dedicated host')
 
 def create_vpc(cidr, tenancy, region):
-    """ Return VPC description after creation """
     ec2 = boto3.client('ec2', region_name=region)
-    response = ec2.create_vpc(CidrBlock=cidr, InstanceTenancy=tenancy)
-    return response
+    ec2_resource = boto3.resource('ec2', region_name=region)
+    vpc_response = ec2.create_vpc(CidrBlock=cidr, InstanceTenancy=tenancy)
+    vpc_id = vpc_response['Vpc']['VpcId']
+    vpc = ec2_resource.Vpc(vpc_id)
+    vpc.wait_until_exists()
+    add_tags(vpc)
 
 if __name__ == '__main__':
     create_vpc()
